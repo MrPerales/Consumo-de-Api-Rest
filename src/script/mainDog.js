66c49a4ecb548ded11edddbf733ee74b2a781896
Carlos Perales <carlosperales050@gmail.com>
2023-03-15 16:26:46 -0600

    cambios

--- src/script/mainDog.js
+++ src/script/mainDog.js
@@ -1,7 +1,8 @@
 const API_KEY = 'api_key=live_1knuvHYlMuFYAhN0YcjJLgAQoGmS9GOZrotiROgapSw9lWoSjTbkK1ROLCsFWCh5'
 const DOG_API_RANDOM = `https://api.thedogapi.com/v1/images/search?limit=2`;
-const DOG_API_FAVORITES = `https://api.thedogapi.com/v1/favourites?${API_KEY}`;
-const DOG_API_FAVORITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=live_1knuvHYlMuFYAhN0YcjJLgAQoGmS9GOZrotiROgapSw9lWoSjTbkK1ROLCsFWCh5`;
+const DOG_API_FAVORITES = `https://api.thedogapi.com/v1/favourites`;
+const DOG_API_FAVORITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}`;
+// const DOG_API_UPLOAD = `https://api.thedogapi.com/v1/images/upload`;
 
 const spanError = document.querySelector('.error');
 
@@ -39,7 +40,12 @@ async function randomDogs() {
 }
 ////////////////
 async function favoritesDogs() {
-    const response = await fetch(DOG_API_FAVORITES);
+    const response = await fetch(DOG_API_FAVORITES,{
+        method:'GET',
+        headers:{
+            'x-api-key':'live_1knuvHYlMuFYAhN0YcjJLgAQoGmS9GOZrotiROgapSw9lWoSjTbkK1ROLCsFWCh5',
+        },
+    });
     const data = await response.json();
     console.log('favorites');
     console.log(data);
@@ -77,7 +83,8 @@ async function saveFavoriteDog(id) {
     const response = await fetch(DOG_API_FAVORITES, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'x-api-key':'live_1knuvHYlMuFYAhN0YcjJLgAQoGmS9GOZrotiROgapSw9lWoSjTbkK1ROLCsFWCh5',
         },
         body: JSON.stringify({
             image_id: id
@@ -99,6 +106,9 @@ async function saveFavoriteDog(id) {
 async function delateFavoriteDog(id) {
     const response = await fetch(DOG_API_FAVORITES_DELETE(id), {
         method: 'DELETE',
+        headers:{
+            'x-api-key':'live_1knuvHYlMuFYAhN0YcjJLgAQoGmS9GOZrotiROgapSw9lWoSjTbkK1ROLCsFWCh5',
+        }
     });
     const data = response.json();
     console.log('Delete');
@@ -113,7 +123,36 @@ async function delateFavoriteDog(id) {
 
 }
 
+async function uploadDogPicture(){
+    const form = document.querySelector('.uploadingForm');
+    const formData= new FormData(form); //add all inputs values from form 
 
+    console.log(formData.get('file'));//show information about the document uploading
+
+    const response = await fetch(DOG_API_UPLOAD ,{
+        method:'POST',
+        headers:{
+            // 'Content-Type': 'multipart/form-data', // we don't use because fetch() have this inside 
+            'x-api-key':'live_1knuvHYlMuFYAhN0YcjJLgAQoGmS9GOZrotiROgapSw9lWoSjTbkK1ROLCsFWCh5',
+        },
+        body:formData,
+    });
+    const data= response.json();
+
+
+    if (response.status !== 200) {
+        spanError.innerHTML = 'Error: ' + response.status;
+    } else {
+        console.log('DONE UPLOAD');
+        console.log({data});
+        console.log(data.url);
+        saveFavoriteDog(data.id);//to save in section favorites after upload the img 
+    }
+
+
+
+}
+////////////////////
 
 randomDogs();
 favoritesDogs();
